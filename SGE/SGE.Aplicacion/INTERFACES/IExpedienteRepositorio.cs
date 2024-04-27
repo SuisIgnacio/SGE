325dfcc6b8c55df3a6f8c7fc6891b4b795832705
Juan Francisco Lorenzo <121961863+FranLorenzoo@users.noreply.github.com>
2024-04-27 12:43:26 -0300

    Add files via upload

---
+++ SGE/SGE.Aplicacion/INTERFACES/IExpedienteRepositorio.cs
@@ -0,0 +1,11 @@
+﻿namespace SGE.Aplicacion
+{
+    public interface IExpedienteRepositorio
+    {
+        public void BajaExpediente(Expediente e);
+        public void AltaExpediente(Expediente e);
+        public Expediente ConsultaPorID(int ID);
+        public List<Expediente> ConsultaPorTodos();
+        public void ModificarExpediente(Expediente e);
+    }
+}
