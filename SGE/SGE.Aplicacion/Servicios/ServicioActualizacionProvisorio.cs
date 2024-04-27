9fe0168d04b05c6fcc03020d6ca6d44c0a155ee9
Gasparsanchez334 <78025076+Gasparsanchez334@users.noreply.github.com>
2024-04-27 13:29:56 -0300

    Update ServicioActualizacionProvisorio.cs

---
+++ SGE/SGE.Aplicacion/Servicios/ServicioActualizacionProvisorio.cs
@@ -0,0 +1,11 @@
+namespace SGE.Aplicacion; 
+
+public class ServicioAutorizacionProvisorio : IServicioAutorizacion
+{
+    public bool autoriza (int num)
+    {
+        if (num == 1){ return true;}
+        
+        else {return false; }
+    }
+}
