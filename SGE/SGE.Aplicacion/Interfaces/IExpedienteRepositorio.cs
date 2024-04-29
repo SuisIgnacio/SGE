67a0577f3311c07836bb61c75033b0125a1fc2c8
SuisIgnacio <126824992+SuisIgnacio@users.noreply.github.com>
2024-04-29 11:10:55 -0300

    Update IExpedienteRepositorio.cs

--- SGE/SGE.Aplicacion/Interfaces/IExpedienteRepositorio.cs
+++ SGE/SGE.Aplicacion/Interfaces/IExpedienteRepositorio.cs
@@ -1,11 +1,10 @@
 namespace SGE.Aplicacion
+
+public interface IExpedienteRepositorio
 {
-    public interface IExpedienteRepositorio
-    {
-        public void BajaExpediente(Expediente e);
-        public void AltaExpediente(Expediente e);
-        public Expediente ConsultaPorID(int ID);
-        public List<Expediente> ConsultaPorTodos();
-        public void ModificarExpediente(Expediente e);
-    }
+    public void BajaExpediente(Expediente e);
+    public void AltaExpediente(Expediente e);
+    public Expediente ConsultaPorID(int ID);
+    public List<Expediente> ConsultaPorTodos();
+    public void ModificarExpediente(Expediente e);
 }
