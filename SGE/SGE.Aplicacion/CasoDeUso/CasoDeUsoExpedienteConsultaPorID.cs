366e15193ca56e8d1a99fdf554f2e9a01972c158
SuisIgnacio <126824992+SuisIgnacio@users.noreply.github.com>
2024-04-29 11:06:37 -0300

    Update CasoDeUsoExpedienteConsultaPorID.cs

---
+++ SGE/SGE.Aplicacion/CasoDeUso/CasoDeUsoExpedienteConsultaPorID.cs
@@ -0,0 +1,9 @@
+namespace SGE.Aplicacion ;
+
+public class CasoDeUsoExpedienteConsultaPorID (IExpedienteRepositorio repo) 
+{
+    public Expediente Ejecutar (in id) 
+    {
+        repo.ConsultarPorID(id) ;
+    }
+}
