d00f540c0837b9fe122d9617f3fb27fad21d449e
SuisIgnacio <126824992+SuisIgnacio@users.noreply.github.com>
2024-05-01 15:52:29 -0300

    Add files via upload

---
+++ SGE/SGE.Repositorios/SGE.Repositorios/RepositorioTramiteTXT.cs
@@ -0,0 +1,26 @@
+namespace SGE.Repositorios;
+using SGE.Aplicacion;
+
+public class RepositorioTramiteTXT:ITramiteRepositorio,IServicioAutorizacion
+{
+    void TramiteBaja(Tramite t)
+    {
+
+    }
+    void TramiteAlta(Tramite t)
+    {
+
+    }
+    List<Tramite> TramiteConsultaPorEtiqueta(EtiquetaTramite i)
+    {
+
+    }
+    void TramiteModificacion(Tramite t)
+    {
+
+    }
+    public Boolean autoriza (int var)
+    {
+        
+    }
+}
