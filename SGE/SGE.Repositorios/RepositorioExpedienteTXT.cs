79e2caf9e9abf18bc38f0d07791fc85e5811568d
SuisIgnacio <126824992+SuisIgnacio@users.noreply.github.com>
2024-05-04 11:24:18 -0300

    Add files via upload

--- SGE/SGE.Repositorios/RepositorioExpedienteTXT.cs
+++ SGE/SGE.Repositorios/RepositorioExpedienteTXT.cs
@@ -6,23 +6,36 @@ public class RepositorioExpedienteTXT: IExpedienteRepositorio
     readonly string _nombreArch="expedientes.txt";
     public void BajaExpediente(int IDExpediente)
     {
+        readonly string archAux="temp.txt";
         using var sr=new StringReader(_nombreArch);
-        using var sw=new StreamWriter(_nombreArch,true);
+        using var sw=new StreamWriter(File.CreateText(archAux),true);
         int IDactual;
         bool found=false;
-        while(!sr.EndOfStream && !found)
+        while(!sr.EndOfStream)
         {
             IDactual= Convert.ToInt32(reader.ReadLine());;
             if(IDactual==IDExpediente)
             {
                 found=true;
-                
+                Console.WriteLine("El Expediente fue eliminado");
             }else{
                 for(int i=0;i<5;i++)
                 {
-                    sr.ReadLine();   
+                    sw.WriteLine(sr.ReadLine());   
                 }
             }
+        }
+        sw.Close();
+        sr.Close();
+        if(!found)
+        {
+            Console.WriteLine("El Expediente no se encontro");
+            File.Delete(archAux);
+        }
+        else
+        {
+            File.Delete(_nombreArch);
+            File.Move(archAux, _nombreArch);
         }
     }
     public void AltaExpediente(Expediente e)
@@ -37,11 +50,41 @@ public class RepositorioExpedienteTXT: IExpedienteRepositorio
     }
     public Expediente ConsultaPorID(int ID)
     {
-
+        using var sr=new StringReader(_nombreArch);
+        Expediente expedienteRetorno=new Expediente();
+        int IDactual;
+        bool found=false;
+        while(!sr.EndOfStream)
+        {
+            IDactual= Convert.ToInt32(sr.ReadLine());;
+            if(IDactual==IDExpediente)
+            {
+                found=true;
+                Console.WriteLine("El Expediente fue encontrado");
+                expedienteRetorno.ID=IDactual;
+                expedienteRetorno.FechaInicio=Convert.DateTime(sr.ReadLine());
+                expedienteRetorno.Caratula=sr.ReadLine();
+                expedienteRetorno.FechaFinalizacion=Convert.DateTime(sr.ReadLine());
+                expedienteRetorno.IDUsuario=Convert.DateTime(sr.ReadLine());
+                expedienteRetorno.Estado=sr.ReadLine();
+            }
+        }
+        if(!found) Console.WriteLine("El Expediente no fue encontrado");
+        return expedienteRetorno;
     }
     public List<Expediente> ConsultaPorTodos()
     {
-
+        using var sr=new StringReader(_nombreArch);
+        List<Expediente> listaExpedientes=new List<Expediente>();
+        while(!sr.EndOfStream)
+        {
+            expedienteRetorno.ID=IDactual;
+            expedienteRetorno.FechaInicio=Convert.DateTime(sr.ReadLine());
+            expedienteRetorno.Caratula=sr.ReadLine();
+            expedienteRetorno.FechaFinalizacion=Convert.DateTime(sr.ReadLine());
+            expedienteRetorno.IDUsuario=Convert.DateTime(sr.ReadLine());
+            expedienteRetorno.Estado=sr.ReadLine();
+        }
     }
     public void ModificarExpediente(Expediente e)
     {
--- SGE/SGE.Repositorios/RepositorioTramiteTXT.cs
+++ SGE/SGE.Repositorios/RepositorioTramiteTXT.cs
@@ -4,9 +4,39 @@ using SGE.Aplicacion;
 public class RepositorioTramiteTXT:ITramiteRepositorio
 {
     readonly string _nombreArch="tramites.txt";
-    public void TramiteBaja(Tramite t)
+    public void TramiteBaja(int IDTramite)
     {
-
+        readonly string archAux="temp.txt";
+        using var sr=new StringReader(_nombreArch);
+        using var sw=new StreamWriter(File.CreateText(archAux),true);
+        int IDactual;
+        bool found=false;
+        while(!sr.EndOfStream)
+        {
+            IDactual= Convert.ToInt32(reader.ReadLine());;
+            if(IDactual==IDTramite)
+            {
+                found=true;
+                Console.WriteLine("El Expediente fue eliminado");
+            }else{
+                for(int i=0;i<6;i++)
+                {
+                    sw.WriteLine(sr.ReadLine());   
+                }
+            }
+        }
+        sw.Close();
+        sr.Close();
+        if(!found)
+        {
+            Console.WriteLine("El Expediente no se encontro");
+            File.Delete(archAux);
+        }
+        else
+        {
+            File.Delete(_nombreArch);
+            File.Move(archAux, _nombreArch);
+        }
     }
     public void TramiteAlta(Tramite t)
     {
