29f3766acb11a3d0a97d238114448445c659b077
Juan Francisco Lorenzo <121961863+FranLorenzoo@users.noreply.github.com>
2024-05-04 12:01:38 -0300

    Update RepositorioExpedienteTXT.cs

--- SGE/SGE.Repositorios/RepositorioExpedienteTXT.cs
+++ SGE/SGE.Repositorios/RepositorioExpedienteTXT.cs
@@ -1,93 +1,123 @@
 namespace SGE.Repositorios;
 using SGE.Aplicacion;
+using System.Reflection.PortableExecutable;
 
-public class RepositorioExpedienteTXT: IExpedienteRepositorio
+public class RepositorioExpedienteTXT : IExpedienteRepositorio
 {
-    readonly string _nombreArch="expedientes.txt";
+    private string path = "expediente.txt";
+    private string pathaux = "aux.txt";
     public void BajaExpediente(int IDExpediente)
     {
-        readonly string archAux="temp.txt";
-        using var sr=new StringReader(_nombreArch);
-        using var sw=new StreamWriter(File.CreateText(archAux),true);
+        using var sr = new StreamReader(path);
+        using var sw = new StreamWriter(pathaux,false);
         int IDactual;
-        bool found=false;
-        while(!sr.EndOfStream)
+        bool found = false;
+        while (!sr.EndOfStream && !found)
         {
-            IDactual= Convert.ToInt32(reader.ReadLine());;
-            if(IDactual==IDExpediente)
+            IDactual = Convert.ToInt32(sr.ReadLine()); ;
+            if (IDactual == IDExpediente)
             {
-                found=true;
-                Console.WriteLine("El Expediente fue eliminado");
-            }else{
-                for(int i=0;i<5;i++)
+                found = true;
+
+            }
+            else
+            {
+                sw.WriteLine(IDactual);
+                for (int i = 0; i < 5; i++)
                 {
-                    sw.WriteLine(sr.ReadLine());   
+                    sw.WriteLine(sr.ReadLine());
                 }
             }
         }
-        sw.Close();
         sr.Close();
-        if(!found)
-        {
-            Console.WriteLine("El Expediente no se encontro");
-            File.Delete(archAux);
-        }
-        else
-        {
-            File.Delete(_nombreArch);
-            File.Move(archAux, _nombreArch);
-        }
+        sw.Close();
+        File.Delete(path);
+        File.Move(pathaux, path);
     }
     public void AltaExpediente(Expediente e)
     {
-        using var sw=new StreamWriter(_nombreArch,true);
-        sw.WriteLine(e.ID);
+        using var sw = new StreamWriter(path, true);
+        sw.WriteLine(e.IDExpediente);
         sw.WriteLine(e.FechaInicio);
         sw.WriteLine(e.Caratula);
-        sw.WriteLine(e.FechaFinalizacion);
+        sw.WriteLine(e.FechaActualizacion);
         sw.WriteLine(e.IDUsuario);
-        sw.WriteLine(e.Estado);
+        sw.WriteLine(Convert.ToInt32(e.Estado));
     }
-    public Expediente ConsultaPorID(int ID)
+    public Expediente ConsultaPorID(int IDExpediente)
     {
-        using var sr=new StringReader(_nombreArch);
-        Expediente expedienteRetorno=new Expediente();
+        using var sr = new StreamReader(path);
+        Expediente expedienteRetorno = new Expediente();
         int IDactual;
-        bool found=false;
-        while(!sr.EndOfStream)
+        bool found = false;
+        while (!sr.EndOfStream)
         {
-            IDactual= Convert.ToInt32(sr.ReadLine());;
-            if(IDactual==IDExpediente)
+            IDactual = Convert.ToInt32(sr.ReadLine()); ;
+            if (IDactual == IDExpediente)
             {
-                found=true;
+                found = true;
                 Console.WriteLine("El Expediente fue encontrado");
-                expedienteRetorno.ID=IDactual;
-                expedienteRetorno.FechaInicio=Convert.DateTime(sr.ReadLine());
-                expedienteRetorno.Caratula=sr.ReadLine();
-                expedienteRetorno.FechaFinalizacion=Convert.DateTime(sr.ReadLine());
-                expedienteRetorno.IDUsuario=Convert.DateTime(sr.ReadLine());
-                expedienteRetorno.Estado=sr.ReadLine();
+                expedienteRetorno.IDExpediente = IDactual;
+                expedienteRetorno.FechaInicio = Convert.ToDateTime(sr.ReadLine());
+                expedienteRetorno.Caratula = sr.ReadLine();
+                expedienteRetorno.FechaActualizacion = Convert.ToDateTime(sr.ReadLine());
+                expedienteRetorno.IDUsuario = Convert.ToInt32(sr.ReadLine());
+                expedienteRetorno.Estado = (EstadoExpediente) Convert.ToInt32(sr.ReadLine());
             }
         }
-        if(!found) Console.WriteLine("El Expediente no fue encontrado");
+        sr.Close();
+        if (!found) Console.WriteLine("El Expediente no fue encontrado");
         return expedienteRetorno;
     }
     public List<Expediente> ConsultaPorTodos()
     {
-        using var sr=new StringReader(_nombreArch);
-        List<Expediente> listaExpedientes=new List<Expediente>();
-        while(!sr.EndOfStream)
+        using var sr = new StreamReader(path);
+        List<Expediente> listaExpedientes = new List<Expediente>();
+        Expediente expedienteRetorno = new Expediente();
+        while (!sr.EndOfStream)
         {
-            expedienteRetorno.ID=IDactual;
-            expedienteRetorno.FechaInicio=Convert.DateTime(sr.ReadLine());
-            expedienteRetorno.Caratula=sr.ReadLine();
-            expedienteRetorno.FechaFinalizacion=Convert.DateTime(sr.ReadLine());
-            expedienteRetorno.IDUsuario=Convert.DateTime(sr.ReadLine());
-            expedienteRetorno.Estado=sr.ReadLine();
+            expedienteRetorno.IDExpediente = Convert.ToInt32(sr.ReadLine());
+            expedienteRetorno.FechaInicio = Convert.ToDateTime(sr.ReadLine());
+            expedienteRetorno.Caratula = sr.ReadLine();
+            expedienteRetorno.FechaActualizacion = Convert.ToDateTime(sr.ReadLine());
+            expedienteRetorno.IDUsuario = Convert.ToInt32(sr.ReadLine());
+            expedienteRetorno.Estado =(EstadoExpediente) Convert.ToInt32 (sr.ReadLine());
+            listaExpedientes.Add(expedienteRetorno);
         }
+        return listaExpedientes;
     }
     public void ModificarExpediente(Expediente e)
     {
-
+        using var sr = new StreamReader(path);
+        using var sw = new StreamWriter(pathaux, false);
+        int IDactual;
+        bool found = false;
+        while (!sr.EndOfStream && !found)
+        {
+            IDactual = Convert.ToInt32(sr.ReadLine()); ;
+            if (IDactual == e.IDExpediente)
+            {
+                found = true;
+                Console.WriteLine("El Expediente fue encontrado");
+                sw.WriteLine(IDactual);
+                sw.WriteLine(e.FechaInicio);
+                sw.WriteLine(e.Caratula);
+                sw.WriteLine(e.FechaActualizacion);
+                sw.WriteLine(e.IDUsuario);
+                sw.WriteLine(e.Estado); 
+            }
+            else
+            {
+                sw.WriteLine(IDactual);
+                for (int i = 0; i < 5; i++)
+                {
+                    sw.WriteLine(sr.ReadLine());
+                }
+            }
+        }
+        sr.Close();
+        sw.Close();
+        File.Delete(path);
+        File.Move(pathaux, path);
     }
 }
