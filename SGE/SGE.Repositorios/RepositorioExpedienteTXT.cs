07fc6c456a9ba29b7fdf419d0c9e5c2e29b109b0
SuisIgnacio <126824992+SuisIgnacio@users.noreply.github.com>
2024-05-04 10:29:43 -0300

    Add files via upload

--- SGE/SGE.Repositorios/RepositorioExpedienteTXT.cs
+++ SGE/SGE.Repositorios/RepositorioExpedienteTXT.cs
@@ -1,15 +1,39 @@
 namespace SGE.Repositorios;
 using SGE.Aplicacion;
 
-public class RepositorioExpedienteTXT: IExpedienteRepositorio,IServicioAutorizacion
+public class RepositorioExpedienteTXT: IExpedienteRepositorio
 {
-    public void BajaExpediente(Expediente e)
+    readonly string _nombreArch="expedientes.txt";
+    public void BajaExpediente(int IDExpediente)
     {
-
+        using var sr=new StringReader(_nombreArch);
+        using var sw=new StreamWriter(_nombreArch,true);
+        int IDactual;
+        bool found=false;
+        while(!sr.EndOfStream && !found)
+        {
+            IDactual= Convert.ToInt32(reader.ReadLine());;
+            if(IDactual==IDExpediente)
+            {
+                found=true;
+                
+            }else{
+                for(int i=0;i<5;i++)
+                {
+                    sr.ReadLine();   
+                }
+            }
+        }
     }
     public void AltaExpediente(Expediente e)
     {
-
+        using var sw=new StreamWriter(_nombreArch,true);
+        sw.WriteLine(e.ID);
+        sw.WriteLine(e.FechaInicio);
+        sw.WriteLine(e.Caratula);
+        sw.WriteLine(e.FechaFinalizacion);
+        sw.WriteLine(e.IDUsuario);
+        sw.WriteLine(e.Estado);
     }
     public Expediente ConsultaPorID(int ID)
     {
@@ -20,10 +44,6 @@ public class RepositorioExpedienteTXT: IExpedienteRepositorio,IServicioAutorizac
 
     }
     public void ModificarExpediente(Expediente e)
-    {
-
-    }
-    public Boolean autoriza (int var)
     {
 
     }
--- SGE/SGE.Repositorios/RepositorioTramiteTXT.cs
+++ SGE/SGE.Repositorios/RepositorioTramiteTXT.cs
@@ -1,15 +1,23 @@
 namespace SGE.Repositorios;
 using SGE.Aplicacion;
 
-public class RepositorioTramiteTXT:ITramiteRepositorio,IServicioAutorizacion
+public class RepositorioTramiteTXT:ITramiteRepositorio
 {
+    readonly string _nombreArch="tramites.txt";
     public void TramiteBaja(Tramite t)
     {
 
     }
     public void TramiteAlta(Tramite t)
     {
-
+        using var sw=new StreamWriter(_nombreArch,true);
+        sw.WriteLine(t.IDTramite);
+        sw.WriteLine(t.IDExpediente);
+        sw.WriteLine(t.Etiqueta);
+        sw.WriteLine(t.FechaCreacion);
+        sw.WriteLine(t.Contenido);
+        sw.WriteLine(t.FechaFinalizacion);
+        sw.WriteLine(t.IDUsuario);
     }
     public List<Tramite> TramiteConsultaPorEtiqueta(EtiquetaTramite i)
     {
@@ -18,9 +26,5 @@ public class RepositorioTramiteTXT:ITramiteRepositorio,IServicioAutorizacion
     public void TramiteModificacion(Tramite t)
     {
 
-    }
-    public Boolean autoriza (int var)
-    {
-        
     }
 }
