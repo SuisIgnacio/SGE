68abb8c7aa97f5a99235b6eea358f7c7b01caa0e
SuisIgnacio <126824992+SuisIgnacio@users.noreply.github.com>
2024-05-06 19:11:14 -0300

    Update ITramiteRepositorio.cs

--- SGE/SGE.Aplicacion/Interfaces/ITramiteRepositorio.cs
+++ SGE/SGE.Aplicacion/Interfaces/ITramiteRepositorio.cs
@@ -2,7 +2,7 @@ namespace SGE.Aplicacion;
 
 public interface ITramiteRepositorio
 {
-    void TramiteBaja(Tramite t);
+    void TramiteBaja(int IDTramite);
     void TramiteAlta(Tramite t);
     List<Tramite> TramiteConsultaPorEtiqueta(EtiquetaTramite i);
     void TramiteModificacion(Tramite t);
