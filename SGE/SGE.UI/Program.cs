8592570f9b6eab3aaf030d16a0b38e7a33c142cc
SuisIgnacio <126824992+SuisIgnacio@users.noreply.github.com>
2024-06-12 17:12:37 -0300

    Add files via upload

---
+++ SGE/SGE.UI/Program.cs
@@ -0,0 +1,48 @@
+using SGE.UI.Components;
+
+using SGE.Aplicacion;
+using SGE.Repositorios;
+
+SGESqlite.Inicializar();
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddTransient<CasoDeUsoExpedienteAlta>();
+builder.Services.AddTransient<CasoDeUsoExpedienteBaja>();
+builder.Services.AddTransient<CasoDeUsoExpedienteConsultaPorID>();
+builder.Services.AddTransient<CasoDeUsoExpedienteConsultaTodos>();
+builder.Services.AddTransient<CasoDeUsoExpedienteModificacion>();
+
+builder.Services.AddTransient<CasoDeUsoTramiteAlta>();
+builder.Services.AddTransient<CasoDeUsoTramiteBaja>();
+builder.Services.AddTransient<CasoDeUsoTramiteConsultaPorEtiqueta>();
+builder.Services.AddTransient<CasoDeUsoTramiteModificacion>();
+
+builder.Services.AddTransient<CasoDeUsoUsuarioAlta>();
+builder.Services.AddTransient<CasoDeUsoUsuarioBaja>();
+builder.Services.AddTransient<CasoDeUsoUsuarioModificacion>();
+builder.Services.AddTransient<CasoDeUsoUsuarioModificacionAdmin>();
+
+builder.Services.AddScoped<IUsuarioRepositorio,RepositorioUsuario>();
+builder.Services.AddScoped<ITramiteRepositorio,RepositorioTramite>();
+builder.Services.AddScoped<IExpedienteRepositorio,RepositorioExpediente>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
