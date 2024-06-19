0001aeee1248889f31b173e8ab029a40d7e8dcdb
SuisIgnacio <126824992+SuisIgnacio@users.noreply.github.com>
2024-06-18 23:03:00 -0300

    Delete SGE/SGE.Console directory

--- SGE/SGE.Console/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿using SGE.Aplicacion;
-using SGE.Repositorios;
-
-RepositorioTramiteTXT repoTramite = new RepositorioTramiteTXT() ;
-var tram = new CasoDeUsoTramiteModificacion (repoTramite); 
-
-tram.Ejecutar (new Tramite (17,5,(EtiquetaTramite)2,"MUJICON",DateTime.Now,DateTime.Now,1)) ;
