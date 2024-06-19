fc149c2f791ee2a73798006d5caa569f83bea71b
SuisIgnacio <126824992+SuisIgnacio@users.noreply.github.com>
2024-06-18 23:01:47 -0300

    Add files via upload

--- SGE/SGE.Aplicacion/Interfaces/IUsuarioRepositorio.cs
+++ SGE/SGE.Aplicacion/Interfaces/IUsuarioRepositorio.cs
@@ -6,6 +6,8 @@ public interface IUsuarioRepositorio
     public void UsuarioBaja(int IdBorrar);
     public void UsuarioModificacion(Usuario u); // un usuario se modifica a si mismo
     public void UsuarioModificacionAdmin(Usuario u); // un usuario trata de modificar a otro
-    public bool LogIn(string correo,string contraseña);
+    public void ModificarPermiso(Usuario u);
+    public Usuario LogIn(string correo,string contraseña);
     public List<Usuario> ConsultarPorTodos();
+    Usuario ConsultarPorId(int id);
 }
