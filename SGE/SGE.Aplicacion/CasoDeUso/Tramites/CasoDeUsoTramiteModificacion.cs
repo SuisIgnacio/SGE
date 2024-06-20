a6223aa5741c0652fa4b07fa0361358c819f6ea8
AguilaSiempre22 <148600288+AguilaSiempre22@users.noreply.github.com>
2024-06-20 15:37:48 -0300

    Update CasoDeUsoTramiteModificacion.cs

--- SGE/SGE.Aplicacion/CasoDeUso/Tramites/CasoDeUsoTramiteModificacion.cs
+++ SGE/SGE.Aplicacion/CasoDeUso/Tramites/CasoDeUsoTramiteModificacion.cs
@@ -4,8 +4,17 @@ public class CasoDeUsoTramiteModificacion(ITramiteRepositorio repo)
 {
     public void Ejecutar (Tramite modificado,Usuario u)
     {
+        ValidadorTramites v=new ValidadorTramites();
         ServicioAutorizacion s=new ServicioAutorizacion();
-        if(s.autoriza(u.permisos,Permiso.TramiteModificacion)){repo.TramiteModificacion(modificado);}
+        if(s.autoriza(u.permisos,Permiso.TramiteModificacion)){
+            if (v.ValidarTramite(modificado)){
+                repo.TramiteModificacion(modificado);
+            }
+            else
+            {
+                throw new ValidacionException () ;
+            }
+        }
         else throw new AutorizacionException("El usuario no contaba con permisos");
 
     }
