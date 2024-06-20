f7780009a2187568a5611afdfe541850e22c008d
SuisIgnacio <126824992+SuisIgnacio@users.noreply.github.com>
2024-06-20 15:29:13 -0300

    Update CasoDeUsoExpedienteModificacion.cs

--- SGE/SGE.Aplicacion/CasoDeUso/Expedientes/CasoDeUsoExpedienteModificacion.cs
+++ SGE/SGE.Aplicacion/CasoDeUso/Expedientes/CasoDeUsoExpedienteModificacion.cs
@@ -5,7 +5,15 @@ public class CasoDeUsoExpedienteModificacion (IExpedienteRepositorio repo)
     public void Ejecutar(Expediente e,Usuario u)
     {
         ServicioAutorizacion s=new ServicioAutorizacion();
-        if(s.autoriza(u.permisos,Permiso.ExpedienteModificacion)){repo.ModificarExpediente(e);}
+        ValidadorExpedientes v=new ValidadorExpedientes();
+        if(s.autoriza(u.permisos,Permiso.ExpedienteModificacion))
+        {
+            if(v.ValidarExpediente(e))
+            {
+                repo.ModificarExpediente(e);
+            }
+            else throw new ValidacionException();
+        }
         else throw new AutorizacionException("El usuario no contaba con permisos");
     }
 }
